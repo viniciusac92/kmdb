ee50eab11b5930463520da6c2953b632957fb393
ViniciusAC29 <vinicius.campello@hotmail.com>
2021-08-12 18:34:26 -0300

    🎨 feat: add models

--- kmdb_app/models.py
+++ kmdb_app/models.py
@@ -1,3 +1,23 @@
+from django.contrib.auth.models import User
 from django.db import models
 
-# Create your models here.
+
+class Movies(models.Model):
+    title = models.CharField(max_length=255)
+    duration = models.CharField(max_length=255)
+    premiere = models.DateField()
+    classification = models.IntegerField()
+    synopsis = models.CharField(max_length=255)
+
+
+class Genres(models.Model):
+    name = models.CharField(max_length=255)
+    movies = models.ManyToManyField(Movies, related_name="genres")
+
+
+class Reviews(models.Model):
+    critic = models.ForeignKey(User, on_delete=models.CASCADE)
+    stars = models.IntegerField()
+    review = models.CharField(max_length=255)
+    spoilers = models.BooleanField()
+    movie = models.ForeignKey(Movies, on_delete=models.CASCADE)
