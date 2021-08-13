d1e04894907a9c7075dfe940b018354457c502a4
ViniciusAC29 <vinicius.campello@hotmail.com>
2021-08-13 20:00:47 -0300

    🎨 feat: improve reviews test function

--- kmdb_app/tests.py
+++ kmdb_app/tests.py
@@ -1,6 +1,7 @@
+from django.contrib.auth.models import User
 from django.test import TestCase
 
-from .models import Genres, Movies
+from .models import Genres, Movies, Reviews
 
 
 class MoviesModelTest(TestCase):
@@ -48,13 +49,6 @@ class GenresModelTest(TestCase):
         cls.genre = Genres.objects.create(
             name=cls.name,
         )
-        # cls.genre.movies.add(Movies.objects.create(
-        #     title="Um Sonho de Liberdade",
-        #     duration="142m",
-        #     premiere="1994-10-14",
-        #     classification=cls.classification,
-        #     synopsis=cls.synopsis,
-        # ))
 
     # Verifica se existem os campos da model e a respectiva tipagem
     def test_it_has_information_fields(self):
@@ -81,3 +75,45 @@ class GenresModelTest(TestCase):
         self.assertEquals(len(movies), self.genre.movies.count())
         for movie in movies:
             self.assertIn(movie, list(self.genre.movies.all()))
+
+
+class ReviewsModelTest(TestCase):
+    @classmethod
+    def setUpTestData(cls):
+        cls.stars = 16
+        cls.review = "O Poderoso Chefão 2 podia ter dado muito certo.."
+        cls.spoilers = True
+        cls.review = Reviews.objects.create(
+            stars=cls.title,
+            review=cls.review,
+            spoilers=cls.spoilers,
+        )
+
+    # Verifica se existem os campos da model e a respectiva tipagem
+    def test_it_has_information_fields(self):
+        self.assertIsInstance(self.review.stars, int)
+        self.assertIsInstance(self.review.review, str)
+        self.assertIsInstance(self.review.spoilers, bool)
+
+    def test_str_models_methods(self):
+        self.assertEquals(f"<Reviews: {self.review.review}>", str(self.review))
+
+    def test_it_can_be_attached_to_multiple_genres(self):
+        movies = [
+            Movies.objects.create(
+                title="Um Sonho de Liberdade",
+                duration="142m",
+                premiere="1994-10-14",
+                classification=16,
+                synopsis="Andy Dufresne é condenado a duas prisões perpétuas...",
+            )
+            for _ in range(3)
+        ]
+        users = [User.objects.create() for _ in range(3)]
+
+        for genre in genres:
+            genre.movies.add(self.movie)
+
+        self.assertEquals(len(genres), self.movie.genres.count())
+        for genre in genres:
+            self.assertIn(genre, list(self.movie.genres.all()))
