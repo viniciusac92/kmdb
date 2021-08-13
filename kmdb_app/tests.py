7c529bd97a4fd2502bf3107f821dd1523a9d1eaf
ViniciusAC29 <vinicius.campello@hotmail.com>
2021-08-13 19:36:31 -0300

    🍱 🎨 feat: add genres tests funtion

--- kmdb_app/tests.py
+++ kmdb_app/tests.py
@@ -3,7 +3,7 @@ from django.test import TestCase
 from .models import Genres, Movies
 
 
-class MoviesModel2Test(TestCase):
+class MoviesModelTest(TestCase):
     @classmethod
     def setUpTestData(cls):
         cls.title = "Um Sonho de Liberdade"
@@ -27,6 +27,9 @@ class MoviesModel2Test(TestCase):
         self.assertIsInstance(self.movie.classification, int)
         self.assertIsInstance(self.movie.synopsis, str)
 
+    def test_str_models_methods(self):
+        self.assertEquals(f"<Movies: {self.movie.title}>", str(self.movie))
+
     def test_it_can_be_attached_to_multiple_genres(self):
         genres = [Genres.objects.create() for _ in range(3)]
 
@@ -36,3 +39,45 @@ class MoviesModel2Test(TestCase):
         self.assertEquals(len(genres), self.movie.genres.count())
         for genre in genres:
             self.assertIn(genre, list(self.movie.genres.all()))
+
+
+class GenresModelTest(TestCase):
+    @classmethod
+    def setUpTestData(cls):
+        cls.name = "Ação"
+        cls.genre = Genres.objects.create(
+            name=cls.name,
+        )
+        # cls.genre.movies.add(Movies.objects.create(
+        #     title="Um Sonho de Liberdade",
+        #     duration="142m",
+        #     premiere="1994-10-14",
+        #     classification=cls.classification,
+        #     synopsis=cls.synopsis,
+        # ))
+
+    # Verifica se existem os campos da model e a respectiva tipagem
+    def test_it_has_information_fields(self):
+        self.assertIsInstance(self.genre.name, str)
+
+    def test_str_models_methods(self):
+        self.assertEquals(f"<Genres: {self.genre.name}>", str(self.genre))
+
+    def test_it_can_be_attached_to_multiple_movies(self):
+        movies = [
+            Movies.objects.create(
+                title="Um Sonho de Liberdade",
+                duration="142m",
+                premiere="1994-10-14",
+                classification=16,
+                synopsis="Andy Dufresne é condenado a duas prisões perpétuas...",
+            )
+            for _ in range(3)
+        ]
+
+        for movie in movies:
+            movie.genres.add(self.genre)
+
+        self.assertEquals(len(movies), self.genre.movies.count())
+        for movie in movies:
+            self.assertIn(movie, list(self.genre.movies.all()))
